a7e455a97e320ff90bf8630d746b12346a9a71b6
Richard Perez <rich2000@email.unc.edu>
2021-10-29 02:24:01 -0400

    Finished the requirements and added comments.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,24 @@
 //Get
-let button = document.querySelector("button");
 let first = "Richard";
 let last = "Perez";
+var button = document.querySelector("button");
+var p = document.querySelector("p");
 
 let hobbies = ["Music", "Video Games", "Writing"];
 
 //Behavior
+console.log(first + " " + last);
+
+//All the things the button needs to do.
+function toggle() {
+  console.log(first + " " + last);
+  p.innerHTML += `<p>Richard Perez</p>`;
+}
+
+//The array loop in the console.
+for (let i = 0; hobbies.length > i; i++) {
+  console.log(hobbies[i]);
+}
 
 //Wire
+button.addEventListener("click", toggle);
