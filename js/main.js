786b2fad3ac91b0af457e2c4fcfc60fbbffa65be
R-H-Perez <R-H-Perez@users.noreply.github.com>
2021-11-04 22:35:12 +0000

    deploy: ebe6e1132e43fce108ce9448a2505d0a84d665e5

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//Get
+//Get (All the variables needed.)
 let first = "Richard";
 let last = "Perez";
 var button = document.querySelector("button");
@@ -6,17 +6,21 @@ var p = document.querySelector("p");
 
 let hobbies = ["Music", "Video Games", "Writing"];
 
-//Behavior
+//Behavior (Log my name out to the console.)
 console.log(first + " " + last);
 
 //All the things the button needs to do.
 function toggle() {
+  //Log to the console.
   console.log(first + " " + last);
+
+  //Create a new paragraph.
   p.innerHTML += `<p>${first} ${last}</p>`;
 }
 
 //The array loop in the console.
 for (let i = 0; hobbies.length > i; i++) {
+  // Log all three hobbies to the console, until i becomes larger than the length of the array.
   console.log(hobbies[i]);
 }
 
