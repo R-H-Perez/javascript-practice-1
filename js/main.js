4bde9099fc44411d57126ace831926d615e11554
R-H-Perez <R-H-Perez@users.noreply.github.com>
2021-11-04 23:30:11 +0000

    deploy: 59c70f95672757a1ec350de0a5e19ecc3e02d700

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
-//Get (All the variables needed.)
+//Get (All the variables needed for the first three steps.)
 let first = "Richard";
 let last = "Perez";
 var button = document.querySelector("button");
 var p = document.querySelector("p");
 
-let hobbies = ["Music", "Video Games", "Writing"];
-
 //Behavior (Log my name out to the console.)
+//Step 1 is satisfied.
 console.log(first + " " + last);
 
 //All the things the button needs to do.
+//Step 2 & 3 are satisfied.
 function toggle() {
-  //Log to the console.
+  //Log my name out to the console, after you click the button consecutively.
   console.log(first + " " + last);
 
-  //Create a new paragraph.
+  //Create a new paragraph entity in HTML, every single time someone clicks the button.
   p.innerHTML += `<p>${first} ${last}</p>`;
 }
 
+//Getting an array of my hobbies.
+//Step 4 is satisfied.
+let hobbies = ["Music", "Video Games", "Writing"];
+
 //The array loop in the console.
+//Step 5 is satisfied.
 for (let i = 0; hobbies.length > i; i++) {
   // Log all three hobbies to the console, until i becomes larger than the length of the array.
   console.log(hobbies[i]);
